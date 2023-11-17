4856a8253a66057c979097213d3bc7c2d707ef75
Tonevanda <up202108863@up.pt>
2023-11-17 20:15:03 +0000

    Turned each product into a component

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,13 +3,8 @@
 @section('content')
 
 @foreach ($products as $product)
-<div class="product">
-    <a href="{{ route('single-product', $product) }}">
-        <h2> {{ $product->name }} </h2>
-        <p> {{ $product->synopsis }} </p>
-        <p> {{ $product->price }} </p>
-    </a>
-</div>
+
+<x-product-card :product="$product" />
 
 @endforeach
 
