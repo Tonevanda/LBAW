68292a4c31056873217f9afe02487dcc858c4bef
up202004513@up.pt <up202004513@up.pt>
2023-11-20 23:13:18 +0000

    css

--- resources/views/user_search.blade.php
+++ resources/views/user_search.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 
-
 @section('content')
 
 @include('partials._search-users')
 
-@foreach($users as $user)
-    
-    <x-user-card :user="$user" />
-
-@endforeach
+<div class="user-container">
+    @foreach($users as $user)
+        <div class="user-box">
+            <x-user-card :user="$user" />
+        </div>
+    @endforeach
+</div>
 
 @endsection
